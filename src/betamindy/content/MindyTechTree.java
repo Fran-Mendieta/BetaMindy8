a323db74e30ba8cc7fc7c0a087728dd6df20d7ef
Fran <87940694+Fran-Mendieta@users.noreply.github.com>
2026-04-22 16:23:29 -0300

    Update MindyTechTree.java

--- src/betamindy/content/MindyTechTree.java
+++ src/betamindy/content/MindyTechTree.java
@@ -18,109 +18,10 @@ public class MindyTechTree{
 
     public static void load(){
         //Shar branch
-        margeNode(mechanicalDrill, () -> {
-            node(isotopeReactor, () -> {
-                node(arcKiln, () -> {
-                    node(lancerKiln);
-                    node(fusionChamber, () -> {
-                        node(scalarFurnace);
-                    });
-                    node(electroRefiner, () -> {
-                        node(siliconCondenser, () -> {
-
-                        });
-                    });
-                });
-            });
-        });
-        margeNode(duo, () -> {
-            node(spectrum, () -> {
-                node(ray, () -> {
-                    node(astro);
-                });
-            });
-            node(nebula, () -> {
-                node(sequence);
-            });
-        });
-        //end region
-
-        margeNode(payloadConveyor, () -> {
-            //todo re-add payload forge blocks when Anuke does so
-        });
-
-        margeNode(laserDrill, () -> {
-            node(drillMini, () -> {
-               node(drillMega);
-            });
-        });
-        margeNode(pneumaticDrill, () -> {
-            node(mynamite, () -> {
-                node(mynamiteLarge);
-            });
-        });
-
-        margeNode(repairTurret, () -> {
-            node(rejuvenator);
-        });
-
-        margeNode(massDriver, () -> {
-            node(payCannon, () -> {
-                node(payCatapult);
-            });
-        });
-
-        margeNode(door, () -> {
-            node(siliconWall, () -> {
-                node(siliconWallLarge);
-            });
-            node(teamWall);
-        });
-        margeNode(copperWall, () -> {
-            node(leadWall, () -> {
-                node(leadWallLarge);
-                node(coalWall, () -> {
-                    node(coalWallLarge);
-                });
-                node(metaglassWall, () -> {
-                    node(metaglassWallLarge);
-                    node(spikeClear);
-                });
-                node(pyraWall, () -> {
-                    node(pyraWallLarge);
-                    node(blastWall, () -> {
-                        node(blastWallLarge);
-                    });
-                    node(spikePyra);
-                });
-            });
-            node(spikeScrap, () -> {
-                node(crusher, () -> {
-                    node(crusherPyra);
-                });
-            });
-        });
-        margeNode(titaniumWall, () -> {
-            node(graphiteWall, () -> {
-                node(graphiteWallLarge);
-            });
-        });
-        margeNode(surgeWall, () -> {
-            node(cryoWall, () -> {
-                node(cryoWallLarge);
-                node(spikeCryo);
-            });
-            node(spikeSurge);
-        });
 
         margeNode(ripple, () -> {
             node(anchor, () -> {
                 node(bermuda);
-            });
-        });
-        margeNode(wave ,() -> {
-            node(fan, () -> {
-                node(fanMega);
             });
         });
         margeNode(tsunami, () -> {
@@ -131,26 +32,6 @@ public class MindyTechTree{
             node(silo, () -> {
                 node(warehouse);
             });
-            node(chest, () -> {
-                node(largeChest);
-            });
-        });
-        margeNode(container, () -> {
-            node(anucoinNode, () -> {
-                node(anucoinSafe, () -> {
-                    node(anucoinVault);
-                });
-                node(cafe);
-                nodePortal(ancientStore, 5);
-                node(tradingPost);
-                node(itemShop, () -> {
-                    node(unitShop);
-                });
-                node(credit, () -> {
-                    node(taxation);
-                    node(brokerage);
-                });
-            });
         });
 
         margeNode(microProcessor, () -> {
@@ -159,103 +40,13 @@ public class MindyTechTree{
                 node(heatSinkLarge);
             });
         });
-        margeNode(memoryCell, () -> {
-            node(linkPin);
-            node(nullifier);
-        });
-
-        margeNode(solarPanel, () -> {
-            node(button, () -> {
-                node(accel);
-                node(buttonLarge);
-                node(pressurePad, () -> {
-                    node(pressurePadLarge);
-                });
-            });
-        });
-
-        node(boostPad, () -> {
-            node(driftPad, () -> {
-                nodePortal(teleportPad, 9);
-            });
-            node(bumper, () -> {
-                node(bumperBlue);
-                node(bumperPlus);
-            });
-            node(claw, () -> {
-                node(phaseClaw);
-            });
-            node(clearPipe, () -> {
-                node(clearDuct);
-            });
-        });
-
-        margeNode(itemBridge, () -> {
-            node(piston, () -> {
-                node(stickyPiston, () -> {
-                    node(cloner);
-                });
-
-                node(sporeSlime, () -> {
-                    node(sporeSlimeSided);
-                    node(surgeSlime);
-                });
-            });
-        });
-
-        margeNode(conveyor, () -> {
-            node(cog, () -> {
-                node(titaniumCog, () -> {
-                    node(plastaniumCog, () -> {
-                        node(spinner, () -> {
-                            node(spinnerInert);
-                        });
-                    });
-                    node(armoredCog);
-                });
-            });
-
-            node(payloadRail, () -> {
-                node(railSorter);
-            });
-        });
-
-        margeNode(battery, () -> {
-            node(discharger);
-            node(capacitor, () -> {
-                node(megaCapacitor);
-            });
-        });
 
         margeNode(incinerator, () -> {
-            node(fireCan, () -> {
-                node(campfire);
-            });
+            node(campfire);
         });
 
         margeNode(logicDisplay, () -> {
-            node(noteBlock, () -> {
-                node(starNoteBlock);
-                node(sfxBlock);
-            });
-            node(pen, () -> {
-                node(colorModule);
-                node(strokeModule);
-            });
-        });
-
-        margeNode(pyratite, () -> {
-            node(crystalPyra);
-        });
-
-        margeNode(blastCompound, () -> {
-            nodeProduce(cryonite, () -> {
-                node(crystalCryo);
-            });
-        });
-
-        margeNode(Liquids.water, () -> {
-            nodeProduce(MindyLiquids.coffee);
+            node(noteBlock);
         });
     }
 
